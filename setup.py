8c8e5363533f03cc9c1cb592214878998f28e99f
Lynte <lillynte@gmail.com>
2016-10-17 19:49:21 +0900

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='ArrayImage',
     version='1.0.0',
+    description='pasting table image of string array to input image',
     url='https://github.com/Lynte/python-ArrayImage',
-    packages='ArrayImage',
-    install_requires='pillow',
+    packages=['ArrayImage'],
+    install_requires=['pillow'],
 )
