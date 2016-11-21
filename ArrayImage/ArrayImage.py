973ca603b3bed49db53e8b8549dce52687dc1b77
Lynte <lillynte@gmail.com>
2016-11-21 14:41:23 +0900

    Format

--- ArrayImage/ArrayImage.py
+++ ArrayImage/ArrayImage.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 import os
 
+
 class TextImage():
 
-    def __init__(self, texts, font='/Library/Fonts/Times New Roman.ttf', 
-            occupancy=1.0, iscenter=True):
+    def __init__(self, texts, font='/Library/Fonts/Times New Roman.ttf',
+                 occupancy=1.0, iscenter=True):
         self.texts = texts
         self.fontname = font
         self.occupancy = occupancy
@@ -16,16 +17,17 @@ class TextImage():
             print('Fontname {} is not exist'.format(self.fontname))
 
         length = len(self.texts)
-        textheight = (y2 - y1)/length
-        img = Image.new('RGBA', (x2-x1, y2-y1), (255, 255, 255, 0))
+        textheight = (y2 - y1) / length
+        img = Image.new('RGBA', (x2 - x1, y2 - y1), (255, 255, 255, 0))
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype(self.fontname, int(textheight*self.occupancy))
-        #make image with texts
+        font = ImageFont.truetype(
+            self.fontname, int(textheight * self.occupancy))
+        # make image with texts
         for i, text in enumerate(self.texts):
-            draw.text((0, textheight*i), text, font=font, fill='#000')
+            draw.text((0, textheight * i), text, font=font, fill='#000')
         centerize = 0.5 if self.center else 1
         pastedimage = image.copy()
-        pastedimage.paste(img, (x1,
-            y1 + int(textheight*(1-self.occupancy)*centerize)), img)
+        pastedimage.paste(
+            img, (x1, y1 + int(textheight * (1 - self.occupancy) * centerize)), img)
 
         return pastedimage
